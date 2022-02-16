65e3074ab77b0416bd218dba91b5f5341de60260
Iakapo410 <93939448+Iakapo410@users.noreply.github.com>
2022-02-16 17:00:39 +0200

    Create UserService.java

---
+++ UserService.java
@@ -0,0 +1,13 @@
+jdbc:mysql:UserDb;
+
+public class UserService {
+   public static void main (string args) {
+   int pass;
+   Connection dbConncection = null;
+   pass = getObject(param_password);
+   if (param_password = pass) 
+   {
+     
+   }
+   }
+}
